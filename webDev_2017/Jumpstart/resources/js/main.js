441aef20f990f7464151c1cb56b1ff4faabba458
John Riccardi <riccjohn@gmail.com>
2017-04-18 21:17:36 -0400

    added instructions to other projects, worked on javascript for Jumpstart

---
+++ webDev_2017/Jumpstart/resources/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    slidesToShow: 1,
+    centermode: true,
+    arrows: false,
+    autoplay: true,
+    draggable: false,
+    autoplaySpeed: 4000,
+  });
+
+  // var $scrollDistance = 0;
+  // .scroll()
+  //
+  // var $scrollTop = $(this).scrollTop();
+  //
+  // if ($scrollTop - $scrollDistance > 50) {
+  //   var $navHeight = $('.navbar').css('height');
+  // }
+
+
+
+
+});
